ba580d3ed3d9b79bbe38429c0ce225e0ead94b20
astewa44 <astewa44@utk.edu>
2017-06-14 03:27:42 -0400

    Switching to C-Sharp, copied the example in.

---
+++ c-sharp/LedStripMessenger/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LedStripMessenger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LedStripMessenger messenger = new LedStripMessenger { RunLoop = true };
+            messenger.Setup();
+            while (messenger.RunLoop) messenger.Loop();
+            messenger.Exit();
+        }
+    }
+}
