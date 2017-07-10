d77adb9ae5cfca16d68dc485fe0ff7c7a168715d
LycanLambda <macdad64@gmail.com>
2017-07-09 22:26:20 -0400

    Fixed millis?

--- Controller/src/millis.h
+++ Controller/src/millis.h
@@ -24,12 +24,12 @@ REMEMBER: Add sei(); after init_millis() to enable global interrupts!
 
 
 
-volatile unsigned long timer1_millis; 
+volatile unsigned long timer2_millis; 
 //NOTE: A unsigned long holds values from 0 to 4,294,967,295 (2^32 - 1). It will roll over to 0 after reaching its maximum value.
 
-ISR(TIMER1_COMPA_vect)
+ISR(TIMER2_COMPA_vect)
 {
-  timer1_millis++;  
+  timer2_millis++;  
 }
 
 void init_millis(unsigned long f_cpu)
@@ -39,14 +39,14 @@ void init_millis(unsigned long f_cpu)
   ctc_match_overflow = ((f_cpu / 1000) / 8); //when timer1 is this value, 1ms has passed
     
   // (Set timer to clear when matching ctc_match_overflow) | (Set clock divisor to 8)
-  TCCR1B |= (1 << WGM12) | (1 << CS11);
+  TCCR2B |= (1 << WGM22) | (1 << CS21);
   
   // high byte first, then low byte
-  OCR1AH = (ctc_match_overflow >> 8);
-  OCR1AL = ctc_match_overflow;
+  OCR2AH = (ctc_match_overflow >> 8);
+  OCR2AL = ctc_match_overflow;
  
   // Enable the compare match interrupt
-  TIMSK1 |= (1 << OCIE1A);
+  TIMSK2 |= (1 << OCIE2A);
  
   //REMEMBER TO ENABLE GLOBAL INTERRUPTS AFTER THIS WITH sei(); !!!
 }
@@ -57,7 +57,7 @@ unsigned long millis ()
  
   // Ensure this cannot be disrupted
   ATOMIC_BLOCK(ATOMIC_FORCEON) {
-    millis_return = timer1_millis;
+    millis_return = timer2_millis;
   }
   return millis_return;
 } 
